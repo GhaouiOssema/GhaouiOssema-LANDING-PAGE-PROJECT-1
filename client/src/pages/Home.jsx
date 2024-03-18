aac052bcf29dcf32ee636e087ee86c3a579a04bd
oussema ghaoui <oghawi220@gmail.com>
2024-03-18 21:10:34 +0100

    project structure client && server

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
             <div className=" h-screen py-20">
                 <Hero />
             </div>
-            <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+            {/* <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
                 <a href="#about">
                     <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
                         <motion.div
@@ -23,7 +23,7 @@ const Home = () => {
                         />
                     </div>
                 </a>
-            </div>
+            </div> */}
         </div>
     );
 };
