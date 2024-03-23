35b5e6f7ceb528e14b9d51b24ca98880d59b503a
oussema ghaoui <oghawi220@gmail.com>
2024-03-23 05:11:36 +0100

    app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react";
-import { motion } from "framer-motion";
+import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { Footer, Loader, NavBar, StarsCanvas } from "./components";
 import { Home, FAQ, NFCTags, HowItWorks, NFCBusinessCards } from "./pages";
+import { useEffect, useState } from "react";
 
 function App() {
     const [showLoader, setShowLoader] = useState(true);
@@ -14,35 +14,40 @@ function App() {
 
         return () => clearTimeout(timer);
     }, []);
-
     return (
-        <div className="bg-black min-h-screen flex justify-center items-center">
-            <motion.div
-                className="absolute inset-0 flex justify-center items-center"
-                initial={{ opacity: 1 }}
-                animate={{ opacity: showLoader ? 1 : 0 }}
-                transition={{ duration: 0.5 }}
-            >
+        <div className="bg-black">
+            {showLoader ? (
                 <Loader />
-            </motion.div>
-            <motion.div
-                className="w-full max-w-screen-xl"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: showLoader ? 0 : 1 }}
-                transition={{ duration: 0.5 }}
-            >
-                <NavBar />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/faq" element={<FAQ />} />
-                    <Route path="/nfc-tags" element={<NFCTags />} />
-                    <Route path="/how-it-works" element={<HowItWorks />} />
-                    <Route path="/nfc-business-cards" element={<NFCBusinessCards />} />
-                    {/* Define other routes */}
-                </Routes>
-<StarsCanvas />
-                <Footer />
-            </motion.div>
+            ) : (
+                <>
+                    <div className="w-full relative z-0 bg-primary">
+                        <div>
+                            <NavBar />
+                        </div>
+                        {/* <div className=" h-screen" /> */}
+                        <div className="container">
+                            <Routes>
+                                <Route path="/" element={<Home />} />
+                                <Route path="/faq" element={<FAQ />} />
+                                <Route
+                                    path="/plaques-nfc"
+                                    element={<NFCTags />}
+                                />
+                                <Route
+                                    path="/comment-ca-marche"
+                                    element={<HowItWorks />}
+                                />
+                                <Route
+                                    path="/cartes-de-visite-nfc"
+                                    element={<NFCBusinessCards />}
+                                />
+                            </Routes>
+                        </div>
+                        <StarsCanvas />
+                        <Footer />
+                    </div>
+                </>
+            )}
         </div>
     );
 }
