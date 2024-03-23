6db139c16a6c054e1fad456fb929ecfa5b48b404
OUSSEMA_GHAOUI28 <75329850+GhaouiOssema@users.noreply.github.com>
2024-03-23 04:58:57 +0100

    transition between loader && app content

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import { Transition } from "@headlessui/react"; // Import Transition from headlessui
 import { Route, Routes } from "react-router-dom";
 import { Footer, Loader, NavBar, StarsCanvas } from "./components";
 import { Home, FAQ, NFCTags, HowItWorks, NFCBusinessCards } from "./pages";
-import { useEffect, useState } from "react";
 
 function App() {
     const [showLoader, setShowLoader] = useState(true);
@@ -14,40 +14,39 @@ function App() {
 
         return () => clearTimeout(timer);
     }, []);
+
     return (
-        <div className="bg-black">
-            {showLoader ? (
+        <div className="bg-black min-h-screen flex justify-center items-center">
+            <Transition
+                show={showLoader}
+                enter="transition-opacity duration-500"
+                enterFrom="opacity-100"
+                enterTo="opacity-0"
+                leave="transition-opacity duration-500"
+                leaveFrom="opacity-0"
+                leaveTo="opacity-100"
+            >
                 <Loader />
-            ) : (
-                <>
-                    <div className="w-full relative z-0 bg-primary">
-                        <div>
-                            <NavBar />
-                        </div>
-                        {/* <div className=" h-screen" /> */}
-                        <div className="container">
-                            <Routes>
-                                <Route path="/" element={<Home />} />
-                                <Route path="/faq" element={<FAQ />} />
-                                <Route
-                                    path="/plaques-nfc"
-                                    element={<NFCTags />}
-                                />
-                                <Route
-                                    path="/comment-ca-marche"
-                                    element={<HowItWorks />}
-                                />
-                                <Route
-                                    path="/cartes-de-visite-nfc"
-                                    element={<NFCBusinessCards />}
-                                />
-                            </Routes>
-                        </div>
-                        <StarsCanvas />
-                        <Footer />
-                    </div>
-                </>
-            )}
+            </Transition>
+            <Transition
+                show={!showLoader}
+                enter="transition-opacity duration-500"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="transition-opacity duration-500"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+            >
+                <div className="w-full max-w-screen-xl">
+                    <NavBar />
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/faq" element={<FAQ />} />
+                        {/* Define other routes */}
+                    </Routes>
+                    <Footer />
+                </div>
+            </Transition>
         </div>
     );
 }
